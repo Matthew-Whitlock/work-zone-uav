684d9380b9fef65bf289445583e3bb50edcb5d66
jofox13221 <jordan.fox@okstate.edu>
2019-04-18 13:28:49 -0500

    adds lat and long to cars
    to do:
    -find the end of the queue
    -test accuracy of trig functions
    -incorporate speed function

--- MultipleVehicles.py
+++ MultipleVehicles.py
@@ -129,6 +129,48 @@ def getOverlaps(rect1, rect2):
 def getAvgOverlap(rect1, rect2):
 	overlaps = getOverlaps(rect1, rect2)
 	return (overlaps[0] + overlaps[1])/2
+	
+#Finds the distance in meters between two gps coordinates
+def gpsToMeters(lat1, lon1, lat2, lon2):
+	#Radius of Earth in KM
+	R = 6378.137 
+	dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
+	dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
+	a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(dLon/2) * math.sin(dLon/2)
+	c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+	d = R * c
+	return d * 1000
+	
+#Finds the GPS coordinate of something using the distance and angle from a known gps coordinate	
+def newGPSfromDist(lat, lon, dist, angle):
+	R = 6378137 
+	new_lat = 180/math.pi*((dist*math.sin(math.pi*angle/180)/R)+lat*math.pi/180)
+	new_lon = 180/math.pi*((dist*math.cos(math.pi*angle/180)/R)+lon*math.pi/180)
+	new_coord = [new_lat, new_lon]
+	return new_coord
+	
+#Changes a pixel location to gps coordinates based off of how many pixels there are, bottom left gps location, and top right gps location
+def pixelsToGPS(x, y, pix_x, pix_y, bl, tr):
+	new_coord = [0, 0]
+	dxgps = tr[0] - bl[0]
+	dygps = tr[1] - bl[1]
+	new_coord[0] = x/pix_x*dxgps + bl[0]
+	new_coord[1] = y/pix_y*dygps + bl[1]
+	return new_coord
+
+#Takes two coordinates at two times to figure out the speed of the object in kilometers per hour
+def getSpeedKPH(coord1, coord2, t1, t2):
+	dist = gpsToMeters(coord1[0], coord1[1], coord2[0], coord2[1])
+	metpersec = dist/(t2 - t1)
+	kph = metpersec/3.6
+	return kph
+	
+#Takes two coordinates at two times to figure out the speed of the object in miles per hour	
+def getSpeedMPH(coord1, coord2, t1, t2):
+	dist = gpsToMeters(coord1[0], coord1[1], coord2[0], coord2[1])
+	metpersec = dist/(t2 - t1)
+	mph = metpersec/2.23694
+	return mph
 
 #This function returns a rectangle with the same center as reference,
 #and with w/l as a weighted average of to_scale and reference, weighted
@@ -188,6 +230,26 @@ class TrackedObj:
 		self.direction = cur_direction
 
 		#self.direction = 0.25*cur_direction + 0.75*self.direction;
+		
+class Drone:
+	def __init__(self, GPS_coord):
+		self.speed = 0
+		#From 0 to 360 with N=90, E = 0, S = 270, and W = 180
+		self.direction = 0
+		self.height = 0
+		self.lat = GPS_coord[0]
+		self.long = GPS_coord[1]
+		self.zoom = 1.0
+		self.camera_angle = 45
+		
+	def updateInfo(self, GPS_coord):
+		self.speed = 0
+		self.direction = 0
+		self.height = 100
+		self.lat = GPS_coord[0]
+		self.long = GPS_coord[1]
+		self.zoom = 1.0
+		self.camera_angle = 45
 
 # List of current tracked objects
 tracked_objs = []
@@ -197,6 +259,10 @@ tracked_objs = []
 ret, frame = cap.read();
 # gets the image size (channel is not used)
 frame_height, frame_width, channel = frame.shape
+# Drone GPS Coordinates
+drone_coord = [36.12390612329048, -97.06877946853638]
+#Get drone information
+drone_inf = Drone(drone_coord)
 
 while (True):
 
@@ -206,7 +272,19 @@ while (True):
 	if ret == False:
 		print("Unable to read any more");
 		break; #Assume for now that we shouldn't keep trying?
-
+	#Gets update information about the drone
+	drone_inf.updateInfo(drone_coord)
+	dist_to_center = drone_inf.height*math.tan(math.pi*drone_inf.camera_angle/180)
+	center_coord = newGPSfromDist(drone_inf.lat, drone_inf.long, dist_to_center, drone_inf.direction)
+	#Distance from camera to center point of picture
+	line_of_sight = drone_inf.height*math.cos(math.pi*drone_inf.camera_angle/180)
+	DFOV = 94
+	diagonal_length = math.tan(math.pi*DFOV/360)*line_of_sight
+	gamma = math.atan(frame_height/frame_width)
+	alpha = diagonal_length/2*math.cos(gamma)
+	beta = diagonal_length/2*math.sin(gamma)
+	bl = newGPSfromDist(center_coord[0],center_coord[1], diagonal_length/2, drone_inf.direction + 90 + gamma)
+	tr = newGPSfromDist(center_coord[0],center_coord[1], diagonal_length/2, drone_inf.direction - 90 + gamma)
 	# start a timer (to see how long processing and display takes)
 	start_t = cv2.getTickCount();
 	
@@ -238,7 +316,6 @@ while (True):
 	# Newly detected objects
 	# Store as x, y, width, height
 	new_detections = []
-	
 	for point in keypoints:
 		centerpoint = point.pt
 		
@@ -475,10 +552,14 @@ while (True):
 			print("Killed for speed")
 			del tracked_objs[i]
 			continue;
-		
+			
+		gps_loc = pixelsToGPS(x,frame_height - y, frame_width, frame_height, bl, tr)
+		latText = "lat:" + str(gps_loc[0])
+		longText = "long:" + str(gps_loc[1])
 		#Draw surviving objects in green.
 		frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2);
-		frame = cv2.putText(frame, "Car", (x, y), annotation_font, 0.4, (0, 255, 0), 1);
+		frame = cv2.putText(frame, latText, (x, y-17), annotation_font, 0.4, (0, 255, 0), 1);
+		frame = cv2.putText(frame, longText, (x, y-5), annotation_font, 0.4, (0, 255, 0), 1);
 
 	# display image
 
@@ -508,6 +589,9 @@ while (True):
 
 	if (key == ord('x')):
 		break;
+	if (key == ord('p')):
+		for i in range(10000000):
+			nothing(0)
 	elif (key == ord('f')):
 		fullscreen = not(fullscreen);
 
